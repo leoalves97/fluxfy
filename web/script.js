f48c2059cfba6dd7fd86252fafbf706b9245a487
Karol Salvador <karolsalvador91@gmail.com>
2026-08-24 21:10:09 -0300

    Ajustar url para o localhost configurado no ambiente para testes locais

--- web/script.js
+++ web/script.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
             feedback.classList.add('hidden');
 
             try {
-                const response = await fetch('http://127.0.0.1:8000/api/cadastro', {
+                const response = await fetch('http://127.0.0.1:8000/api/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ email: email, password: password })
