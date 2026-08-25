425ab0a584b3d1f5aa0508ccf7eb482fdc6f1d70
Karol Salvador <karolsalvador91@gmail.com>
2026-08-25 06:29:27 -0300

    feature: Capturar os dados de solicitação de cadastro e salvar no banco de dados. Criar função para listar as solicitações pendentes para ser consumida pelo frontend

--- api/main.py
+++ api/main.py
@@ -5,6 +5,14 @@ from passlib.context import CryptContext
 from database import engine, SessionLocal
 import models
 
+# Função para criar uma sessão de banco de dados por requisição
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 # Recria as tabelas no Supabase
 models.Base.metadata.create_all(bind=engine)
 
@@ -25,9 +33,31 @@ class UsuarioCreate(BaseModel):
     password: str
 
 @app.post("/api/cadastro")
-def cadastrar_usuario(usuario: UsuarioCreate):
-    print(f"UHUL! Chegou do front: Nome={usuario.nome}, Email={usuario.email}")
-    return {"status": "sucesso"}
+def cadastrar_usuario(usuario: UsuarioCreate, db = Depends(get_db)):
+    # Verificar se o e-mail já está cadastrado
+    usuario_existente = db.query(models.Usuario).filter(models.Usuario.email == usuario.email).first()
+    if usuario_existente:
+        #Se já tiver retorna 409 (conflito)
+        raise HTTPException(status_code=409, detail="Este e-mail já possui uma solicitação.")
+
+    # Criptografar a senha
+    senha_segura = pwd_context.hash(usuario.password)
+
+    # Preparar dados para salvar
+    novo_usuario = models.Usuario(
+        nome=usuario.nome,
+        email=usuario.email,
+        senha_hash=senha_segura,
+        papel="user",  # Papel padrão para novos usuários
+        aprovado=False
+    )
+
+    # Enviar para Supabase
+    db.add(novo_usuario)
+    db.commit()
+    db.refresh(novo_usuario) # Atualizar a variavel com o id gerado peo banco
+
+    return {"status": "sucesso", "mensagem": "Solicitaçao enviada para análise."}
 
 # Configuração para verificar a senha criptografada
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -67,3 +97,13 @@ def login(usuario: UsuarioLogin, db = Depends(get_db)):
     
     # (Usaremos um token fictício agora, no futuro implementamos um JWT real)
     return {"token": "token_super_secreto_123"}
+
+
+# Função para listar solicitações pendentes no banco de dados para apresentação no frontend
+@app.get("/api/usuarios/pendentes")
+def listar_pendentes(db = Depends(get_db)):
+    # Faz um SELECT na tabela onde aprovado é False
+    usuarios_pendentes = db.query(models.Usuario).filter(models.Usuario.aprovado == False).all()
+    
+    # O FastAPI converte automaticamente essa lista para JSON
+    return usuarios_pendentes
